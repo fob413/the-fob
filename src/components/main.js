44a43191cc4b6ce0fe2bc4063cdfa0592b826912
fob1493 <fob1493@gmail.com>
2020-02-08 23:53:32 +0100

    about page

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Intro from './intro';
+import About from "./about";
 
 
 export default function Main() {
     return (
         <div>
             <Intro/>
+            <About/>
         </div>
     )
 }
