ae23cceab908cf89c060f84a8e5e6e138b457697
fob1493 <fob1493@gmail.com>
2020-02-14 16:43:45 +0100

    Make few adjustments on the UI

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -34,10 +34,7 @@ export class Header extends React.Component {
                                 <li className="current"><a className="smoothscroll" href="#intro" title="">Home</a></li>
                                 <li><a className="smoothscroll" href="#about" title="">About</a></li>
                                 <li><a className="smoothscroll" href="#resume" title="">Resume</a></li>
-                                <li><a className="smoothscroll" href="#portfolio" title="">Portfolio</a></li>
-                                <li><a className="smoothscroll" href="#services" title="">Services</a></li>
                                 <li><a className="smoothscroll" href="#contact" title="">Contact</a></li>
-                                <li><a href="styles.html" title="">Style Demo</a></li>
                             </ul>
                         </nav>
                     </div>
